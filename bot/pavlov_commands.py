3fe609b19ada87a6bfd56650819f718ded28210a
Eva <scienceandfire66@gmail.com>
2023-01-06 09:23:25 -0500

    added more commnds

--- bot/pavlov_commands.py
+++ bot/pavlov_commands.py
@@ -31,27 +31,7 @@ class pavlov_rcon(commands.Cog):
         await ctx.send(command)
         await rcon.close()
     
-    @commands.command()
-    async def rconhelp(self,ctx):
-                
-        f = open("server.txt", "r")
-        ip = f.readline()
-        port = f.readline()
-        password = f.readline()
-        
-        f.close()
-        
-        #connects to the server
-        rcon = PavlovRCON(ip.strip(), port.strip(), password.strip())
-        
-        
-        command = await rcon.send("help")
-        if command["Successful"] == False:
-            await ctx.send("failed to connect to the server")
-            return
-        
-        await ctx.send(command["Help"])
-        await rcon.close()
+
     @commands.command()
     async def kick(self, ctx, steamid = None):
         if steamid == None:
@@ -223,3 +203,135 @@ class pavlov_rcon(commands.Cog):
         else:
             await ctx.send("killed player " + steamid)
         await rcon.close()
+    @commands.command()
+    @commands.is_owner()
+    async def addmod(self, ctx, *, steamid = None):
+        if steamid == None:
+            await ctx.send("please provide a steamid")
+            return
+        f = open("server.txt", "r")
+        ip = f.readline()
+        port = f.readline()
+        password = f.readline()
+        
+        f.close()
+        
+        #connects to the server
+        rcon = PavlovRCON(ip.strip(), port.strip(), password.strip())
+        
+        
+        command = await rcon.send("AddMod " + steamid)
+        if command["Successful"] == False:
+            await ctx.send("failed to connect to the server")
+            return
+        
+        else:
+            await ctx.send("added moderator " + steamid)
+        await rcon.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def removemod(self, ctx, *, steamid = None):
+        if steamid == None:
+            await ctx.send("please provide a steamid")
+            return
+        f = open("server.txt", "r")
+        ip = f.readline()
+        port = f.readline()
+        password = f.readline()
+        
+        f.close()
+        
+        #connects to the server
+        rcon = PavlovRCON(ip.strip(), port.strip(), password.strip())
+        
+        
+        command = await rcon.send("RemoveMod " + steamid)
+        if command["Successful"] == False:
+            await ctx.send("failed to connect to the server")
+            return
+        else:
+            await ctx.send("Removed moderator " + steamid)
+        await rcon.close()
+    @commands.command()
+    async def rotatemap(self, ctx):
+        #checks if the user has asmin role
+        f = open("roles.txt", "r")
+        admin_role = f.readline()
+        f.close()
+        
+        if admin_role.strip() in [y.name for y in ctx.author.roles]:
+            pass
+        else:
+            await ctx.send("you do not have permission to use this command")
+            return
+        f = open("server.txt", "r")
+        ip = f.readline()
+        port = f.readline()
+        password = f.readline()
+        
+        f.close()
+        
+        #connects to the server
+        rcon = PavlovRCON(ip.strip(), port.strip(), password.strip())
+        
+        
+        command = await rcon.send("RotateMap")
+        if command["Successful"] == False:
+            await ctx.send("failed to connect to the server")
+            return
+        
+        else:
+            await ctx.send("Rotated map")
+        await rcon.close()
+    @commands.command()
+    async def maplist(self, ctx):
+        f = open("server.txt", "r")
+        ip = f.readline()
+        port = f.readline()
+        password = f.readline()
+        
+        f.close()
+        
+        #connects to the server
+        rcon = PavlovRCON(ip.strip(), port.strip(), password.strip())
+        
+        
+        command = await rcon.send("MapList")
+        if command["Successful"] == False:
+            await ctx.send("failed to connect to the server")
+            return
+        
+        else:
+            await ctx.send(command["MapList"])
+        await rcon.close()
+    @commands.command()
+    @commands.is_owner()
+    async def switcmap(self, ctx , mapid = None, gamemode = None):
+        if mapid == None:
+            await ctx.send("please provide a mapid")
+            return
+        if gamemode == None:
+            await ctx.send("please provide a gamemode")
+            return
+        f = open("server.txt", "r")
+        ip = f.readline()
+        port = f.readline()
+        password = f.readline()
+
+        f.close()
+
+        #connects to the server
+        rcon = PavlovRCON(ip.strip(), port.strip(), password.strip())
+
+
+        command = await rcon.send("SwitchMap " + mapid + " " + gamemode)
+        if command["Successful"] == False:
+            await ctx.send("failed to connect to the server")
+            return
+        
+        else:
+            await ctx.send("Switched map to " + mapid + " " + gamemode)
+        await rcon.close()
+
+
